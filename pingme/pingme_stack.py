25b48a12effa649b1f9f281067dc1631a0f8872d
Minh Mai <minhmai@Minhs-Personal-Laptop.local>
2021-03-07 21:35:04 -0500

    got cdk ls to work

--- pingme/pingme_stack.py
+++ pingme/pingme_stack.py
@@ -5,11 +5,52 @@ from aws_cdk import core as cdk
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+from aws_cdk import aws_apigateway as apigw
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subs
+from aws_cdk import aws_sqs as sqs
+
+DEFAULT_ENV = {
+    "CGO_ENABLED": "0",
+    "GOOS": "darwin",
+    "GOARCH": "amd64"}
 
 
 class PingmeStack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        go_lambda = _lambda.Function(
+            self,
+            "handler",
+            runtime=_lambda.Runtime.GO_1_X,
+            code=_lambda.Code.from_asset(
+                "lambda",
+                bundling={
+                    "image": _lambda.Runtime.GO_1_X.bundling_docker_image,
+                    "user": "root",
+                    "environment": DEFAULT_ENV,
+                    "command": [
+                        "bash",
+                        "-c",
+                        " && ".join(["make pingme", "make lambda-build"]),
+                    ],
+                }),
+                handler="handler",
+                environment=DEFAULT_ENV,
+        )
+        apigw.LambdaRestApi(
+            self,
+            "Endpoint",
+            handler=go_lambda,
+        )
+        queue = sqs.Queue(
+            self,
+            "SMSQueue",
+            visibility_timeout=core.Duration.seconds(300),
+        )
+
+        topic = sns.Topic(self, "SMS")
+
+        topic.add_subscription(subs.SqsSubscription(queue))
