c78b64ad4d681c746f4ca286d748c14d5a4cd47d
Minh Mai <minhmai@Minhs-Personal-Laptop.local>
2021-04-03 16:33:17 -0400

    got it to work with eventbridge (kinda)

--- lambda/pingme.py
+++ lambda/pingme.py
@@ -1,5 +1,13 @@
 import logging
+import json
 
 def handler(event, context):
     logging.info(event)
-
+    payload = {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': event["body"]
+    }
+    return payload
--- pingme/pingme_stack.py
+++ pingme/pingme_stack.py
@@ -1,9 +1,12 @@
+import os
+
 from aws_cdk import core as cdk
 from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_sns as sns
 from aws_cdk import aws_sns_subscriptions as subs
-from aws_cdk import aws_sqs as sqs
+from aws_cdk import aws_events as events
+import aws_cdk.aws_events_targets as targets
 
 
 
@@ -11,23 +14,34 @@ class PingmeStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # lambda invocation via API`
         py_lambda = _lambda.Function(
             self,
             "handler",
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset("lambda"),
-            handler="handler",
+            handler="pingme.handler",
         )
         apigw.LambdaRestApi(
             self,
             "Endpoint",
             handler=py_lambda,
         )
-        queue = sqs.Queue(
-            self,
-            "SMSQueue",
-            visibility_timeout=cdk.Duration.seconds(300),
-        )
 
-        topic = sns.Topic(self, "SMS")
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # adding sns topics with SMS support
+        phone_num = os.environ.get('PHONE_NUMBER')
+        topic = sns.Topic(
+            self,
+            "SMS")
+        sms_sub = subs.SmsSubscription(phone_num)
+        topic.add_subscription(sms_sub)
+
+        # hooking it up with eventbridge
+        sms_target = targets.SnsTopic(topic)
+        sms_pattern = events.EventPattern(detail=[{"source": ["aws.apigateway"]}])
+        event_rule = events.Rule(
+            self,
+            "sms_rule",
+            targets=[sms_target],
+            event_pattern=sms_pattern
+        )
