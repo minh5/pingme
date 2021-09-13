c287dfcf6cf6f364d5f173ea936597cfab63d21b
Minh Mai <minhmai@minhspeallaptop.lan>
2021-09-13 15:31:32 -0400

    fake testing

--- lambda/pingme.py
+++ lambda/pingme.py
@@ -26,12 +26,37 @@ def handler(event: Dict[str, Any], context: Dict[str, Any]) -> None:
     phone = os.environ["PHONE_NUMBER"]
     obfuscated = "X" * (len(phone) - 4) + phone[-4:]
 
+    data = """
+    HELLO THIS THE FIRST CHALLENGE, parse this data frame
+
+    +-----------+------------+----------+
+    | 1 COLUMN  |  2 COLUMN  | 3 COLUMN |
+    +-----------+------------+----------+
+    | fake_id_1 | fake_id_2  |          |
+    |           |            |          |
+    | 5H22WPQY  | RZ0O44BBSL |          |
+    |           |            |          |
+    | QXX7AXCB  | N9UP27D7MZ |          |
+    |           |            |          |
+    | F1EW26N3  | SDI9N0TNH  |          |
+    |           |            |          |
+    | CX05VC50  | QO3QU2NJJ9 |          |
+    |           |            |          |
+    | QFVHGFWU  | LYXOBRKU3H |          |
+    |           |            |          |
+    | Z8E7WRIP  | MEOQQ91JN8 |          |
+    |           |            |          |
+    | 38ZNGKNM  | GLU8ORMSEP |          |
+    |           |            |          |
+    | B6ULQT9W  | 40RET4EK1E |          |
+    +-----------+------------+----------+
+    """
     # validation
     try:
         # sns_client.publish(**message)
         response["statusCode"] = 200
         # response["body"] = f"message successfully sent via pingme to {obfuscated}"
-        response["body"] = json.dumps(event["body"])
+        response["body"] = json.dumps(data)
     except Exception as e:
         response["statusCode"] = 400
         response["body"] = e
